fd2cd9880e7c5d5550a253e397fbf389fd7715ca
Andrii <xlavreniuk@gmail.com>
2026-06-12 13:44:04 +0200

    Initial Commit

---
+++ SpatialAgent/SpatialAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialAgentApp.swift
+//  SpatialAgent
+//
+//  Created by Andrii on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpatialAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
